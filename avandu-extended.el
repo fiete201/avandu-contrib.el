aeffd5202fc083de8893d63b89da277ae4caf51d
Fritz Reichwald <mail@fritzreichwald.de>
2017-02-02 00:18:58 +0100

    Add needed Columns and default sort order

--- avandu-extended.el
+++ avandu-extended.el
@@ -1,28 +1,57 @@
+;;; avandu-extended.el extension of avandu
+;;; this code adds a new mode for avandu ttrss reader
 (require 'avandu)
-(define-derived-mode avandu2-mode tabulated-list-mode "Avandu2"
-  "Major mode for ttrss displaying headlines in tabulated list.
-\\{avandu2-mode-map}"
-  (setq tabulated-list-format [("Col1" 24 t)
-			       ("Col2" 80 t)
-			       ("Col3"  10 nil)
-			       ("Col4" 10 nil)])
-  (setq tabulated-list-padding 2)
-  (setq tabulated-list-sort-key (cons "Col3" nil))
-  (tabulated-list-init-header))
-
 (defun print-current-line-id ()
   (interactive)
   (message (concat "current line ID is: " (tabulated-list-get-id))))
 
-(defun my-listing-command ()
+(defun avandu2-view-article (id)
+  "Show a single article identified by ID in a new buffer."
+  (interactive "nArticle id: ")
+  (let* ((data (avandu-get-article id))
+         (buffer (get-buffer-create "*avandu-article*"))
+         (inhibit-read-only t)
+         content-start
+         content-end)
+    (with-current-buffer buffer
+      (erase-buffer)
+      (mapc #'(lambda (item)
+                (insert
+                 (propertize (avu-prop item title)
+                             'face 'avandu-article-title))
+                (newline)
+                (insert
+                 (propertize (concat "by: " (avu-prop item author))
+                             'face 'avandu-article-author))
+                (insert " (")
+                (insert-button
+                 "Browse original"
+                 'url (avu-prop item link)
+                 'action #'(lambda (button)
+                             (browse-url (button-get button 'url))))
+                (insert ")")
+                (newline)(newline)
+                (setq content-start (point))
+                (insert (avu-prop item content))
+                (setq content-end (point))
+                (newline)(newline))
+            data)
+      (setq buffer-read-only t)
+      (goto-char (point-min))
+      (avandu-article-mode))
+    (avandu-mark-article-read id)
+    (switch-to-buffer buffer)
+    (when avandu-article-render-function
+      (funcall
+       avandu-article-render-function content-start
+       (min content-end (point-max))))
+    (goto-char (point-min))))
+
+
+(defun avandu2-open-article ()
   (interactive)
-  (pop-to-buffer "*Avandu2 Overview*" nil)
-  (avandu2-mode)
-  (setq tabulated-list-entries (list 
-				(list "1" ["1" "2" "3" "4"])
-				(list "2" ["a" "b" "c" "d"])
-				(list "3" (vector "e" "f" "g" "h"))))
-  (tabulated-list-print t))
+  "Get id of article in focused line and pass it to avandu-view-article"
+  (avandu2-view-article (tabulated-list-get-id)))
 
 (defun avandu-table-overview ()
   "Request the headlines of unread articles and list them.
@@ -41,17 +70,14 @@ meaningless, but it's easy."
       (setq buffer-read-only nil)
       (erase-buffer)
       (goto-char (point-min))
-      (setq tabulated-list-entries (list 
-				(list "1" ["1" "2" "3" "4"])
-				(list "2" ["a" "b" "c" "d"])))
+      (setq tabulated-list-entries '())
       (mapc #'(lambda (elt)
 		(unless (equal feed-id (assq 'feed_id elt))
 		  (add-to-list 'tabulated-list-entries
 			       (list (avu-prop elt id) (vector
 							(avu-prop elt feed_title)
 							(avu-prop elt title)
-							(avu-prop elt excerpt)
-							(avu-prop elt link)
+							(number-to-string (avu-prop elt updated))
 							)))))
 	    result)
       (setq buffer-read-only t)
@@ -60,4 +86,22 @@ meaningless, but it's easy."
       (tabulated-list-print t))
     (switch-to-buffer buffer)))
 
+(defvar avandu2-overview-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map special-mode-map)
+    (define-key map "o" 'avandu2-open-article)
+    (define-key map "q" 'bury-buffer)
+    map)
+  "Keymap for `avandu2-mode'.")
+
+(define-derived-mode avandu2-mode tabulated-list-mode "Avandu2"
+  "Major mode for ttrss displaying headlines in tabulated list.
+\\{avandu2-mode-map}"
+  (use-local-map avandu2-overview-map)
+  (setq tabulated-list-format [("Kategorie" 40 t)
+			       ("Headline" 64 t)
+			       ("Updated" 40 t)])
+  (setq tabulated-list-padding 2)
+  (setq tabulated-list-sort-key (cons "Updated" t))
+  (tabulated-list-init-header))
 
